b8769b093212962039491f03ae5652b8fae52437
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-07-17 08:01:23 +0200

    Part 4 - implement that only letters will be encoded/decoded.

--- main.py
+++ main.py
@@ -13,26 +13,24 @@ while shift > 25:
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     for letter in start_text:
-        position = alphabet.index(letter)
-        if cipher_direction == "encode":
-            shifted_position = position + shift_amount
-            if shifted_position > len(alphabet):
-                shifted_position -= len(alphabet)
-            end_text += alphabet[shifted_position]
-        elif cipher_direction == "decode":
-            shifted_position = position - shift_amount
-            if shifted_position < 0:
-                shifted_position += len(alphabet)
-            end_text += alphabet[shifted_position]
+        if letter.isalpha():
+            position = alphabet.index(letter)
+            if cipher_direction == "encode":
+                shifted_position = position + shift_amount
+                if shifted_position > len(alphabet):
+                    shifted_position -= len(alphabet)
+                end_text += alphabet[shifted_position]
+            elif cipher_direction == "decode":
+                shifted_position = position - shift_amount
+                if shifted_position < 0:
+                    shifted_position += len(alphabet)
+                end_text += alphabet[shifted_position]
+        else:
+            end_text += letter
     print(f"The {cipher_direction}d text is {end_text}")
 
 
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-
-#TODO-3: What happens if the user enters a number/symbol/space?
-#Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-#e.g. start_text = "meet me at 3"
-#end_text = "•••• •• •• 3"
 
 #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
